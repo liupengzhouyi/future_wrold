53e55da689a182d850d31f6183fdc5397c35945e
liupeng <liupeng.0@outlook.com>
2020-05-07 17:39:57 +0800

    临时页面 V 1.0.11

---
+++ lib/utils/page/TempPage.dart
@@ -0,0 +1,95 @@
+
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:animatedloginbutton/animatedloginbutton.dart';
+import 'package:flutter/material.dart';
+import 'package:futurewrold/model/user/UserInformation.dart';
+import 'package:path_provider/path_provider.dart';
+
+void main() => runApp(new TempPage(title: "临时页面",));
+
+class TempPage extends StatelessWidget {
+
+  String title;
+
+
+  @override
+  Widget build(BuildContext context) {
+    // _readCounter();
+    return new MaterialApp(
+      title: 'Welcome to Flutter',
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Welcome to Flutter'),
+        ),
+        body: new Center(
+          child: new Text(title),
+        ),
+      ),
+    );
+
+  }
+
+  TempPage({this.title});
+
+
+
+  // _getLocalFile函数，获取本地文件目录
+  Future<File> _getLocalFile() async {
+    // 获取本地文档目录
+    String dir = (await getApplicationDocumentsDirectory()).path;
+    print(dir);
+    // 返回本地文件目录
+    return new File('$dir/userInformation.txt');
+  }
+
+  // 保存登陆数据
+  void saveValue(UserInformation userInformation) async {
+    try {
+      File f = await _getLocalFile();
+      IOSink slink = f.openWrite(mode: FileMode.append);
+      slink.write('${userInformation.toString()}');
+      slink.close();
+    } catch (e) {
+      // 写入错误
+      print(e);
+    }
+  }
+
+  /*
+   * _readCounter函数，读取点击数
+   * 关键字async表示异步操作
+   * 返回值Future类型，表示延迟处理的对象
+   */
+  void _readCounter() async {
+    try {
+      /*
+       * 获取本地文件目录
+       * 关键字await表示等待操作完成
+       */
+      File file = await _getLocalFile();
+      var dir_bool = await file.exists();
+      if (dir_bool) {
+        print('true');
+      } else {
+        print('false');
+        file.create();
+        UserInformation userInformation = new UserInformation();
+        userInformation.landing = 0;
+        saveValue(userInformation);
+        // this.initState();
+      }
+      // 从文件中读取变量作为字符串，一次全部读完存在内存里面
+      var contents = await file.readAsString();
+      print("=====---- :");
+      print(contents);
+      print("=====---- :");
+      var jsonMap = await json.decode(contents);
+      UserInformation userInformation = UserInformation.fromJson(jsonMap);
+      print("=====---- :" + userInformation.toString());
+    } on FileSystemException {
+    }
+  }
+
+}
