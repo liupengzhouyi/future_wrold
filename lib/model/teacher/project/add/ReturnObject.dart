d5d53eb36f70d51c2b7949f798e26bf30d5b7e72
liupeng <liupeng.0@outlook.com>
2020-05-11 23:24:52 +0800

    教师添加毕设题目 V 1.7.1

---
+++ lib/model/teacher/project/add/ReturnObject.dart
@@ -0,0 +1,36 @@
+class ReturnObject {
+  int id;
+  int teacherid;
+  String title;
+  String fileurl;
+  int isselect;
+  String studentnumber;
+
+  ReturnObject(
+      {this.id,
+        this.teacherid,
+        this.title,
+        this.fileurl,
+        this.isselect,
+        this.studentnumber});
+
+  ReturnObject.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    teacherid = json['teacherid'];
+    title = json['title'];
+    fileurl = json['fileurl'];
+    isselect = json['isselect'];
+    studentnumber = json['studentnumber'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['teacherid'] = this.teacherid;
+    data['title'] = this.title;
+    data['fileurl'] = this.fileurl;
+    data['isselect'] = this.isselect;
+    data['studentnumber'] = this.studentnumber;
+    return data;
+  }
+}
