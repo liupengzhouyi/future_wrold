58d19d150bdef8a1d70b589ad4798a49a9e52407
liupeng <liupeng.0@outlook.com>
2020-05-14 00:52:27 +0800

    题目申请信息页面.请求参数实体 V 1.10.1

---
+++ lib/model/student/project/applicationInformation/Applicationinformation.dart
@@ -0,0 +1,36 @@
+class ApplicationInformation {
+  String applicationdata;
+  int id;
+  int pass;
+  String passdata;
+  int studentid;
+  int titleid;
+
+  ApplicationInformation(
+      {this.applicationdata,
+        this.id,
+        this.pass,
+        this.passdata,
+        this.studentid,
+        this.titleid});
+
+  ApplicationInformation.fromJson(Map<String, dynamic> json) {
+    applicationdata = json['applicationdata'];
+    id = json['id'];
+    pass = json['pass'];
+    passdata = json['passdata'];
+    studentid = json['studentid'];
+    titleid = json['titleid'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['applicationdata'] = this.applicationdata;
+    data['id'] = this.id;
+    data['pass'] = this.pass;
+    data['passdata'] = this.passdata;
+    data['studentid'] = this.studentid;
+    data['titleid'] = this.titleid;
+    return data;
+  }
+}
---
+++ lib/view/student/project/applicationInformation/ApplicationInformationPage.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ApplicationInformationPage extends StatefulWidget {
+  @override
+  _ApplicationInformationPageState createState() => _ApplicationInformationPageState();
+}
+
+class _ApplicationInformationPageState extends State<ApplicationInformationPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
