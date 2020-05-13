3516d5c36c26a84b8d1bc7d39be1c877e3826033
liupeng <liupeng.0@outlook.com>
2020-05-13 23:04:46 +0800

    查询教师数据.请求数据 V 1.8.8

--- lib/view/student/teacher/information/teacherInformationPage.dart
+++ lib/view/student/teacher/information/teacherInformationPage.dart
@@ -1,13 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:futurewrold/model/student/teacher/information/SelectTeacherByNumber.dart';
+import 'package:futurewrold/utils/web/HttpUtils.dart';
 
 class TeacherInformationPage extends StatefulWidget {
+
+  String teachernumber;
+
+
+  TeacherInformationPage(this.teachernumber);
+
   @override
-  _TeacherInformationPageState createState() => _TeacherInformationPageState();
+  _TeacherInformationPageState createState() => _TeacherInformationPageState(this.teachernumber);
 }
 
 class _TeacherInformationPageState extends State<TeacherInformationPage> {
+
+  _TeacherInformationPageState(this.teachernumber);
+
+  String teachernumber;
+
+  SelectTeacherByNumber selectTeacherByNumber = new SelectTeacherByNumber();
+
+
+  @override
+  void initState() {
+    selectTeacherByNumber.teachernumber = this.teachernumber;
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Icon(Icons.access_alarms, color: Colors.lightGreen, size: 28,),
+    );
   }
+
+  Future<void> getData() async {
+    var result = await HttpUtils.request(
+        '/teacher/selectByNumber',
+        method: HttpUtils.POST,
+        data: selectTeacherByNumber.toJson(),
+    );
+    print(result);
+
+
+  }
+
+
+
 }
