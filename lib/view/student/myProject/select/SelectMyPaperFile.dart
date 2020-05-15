42d3b79650564f32eb0ae002032c4e2d7879eed3
liupeng <liupeng.0@outlook.com>
2020-05-15 22:09:28 +0800

    学生查看自己论文文件.返回数据实体 V 1.13.2

--- lib/view/student/myProject/select/SelectMyPaperFile.dart
+++ lib/view/student/myProject/select/SelectMyPaperFile.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:futurewrold/model/student/myProject/select/ReturnObject.dart';
+import 'package:futurewrold/model/student/myProject/select/ReturnSelectMyPaperFileEntity.dart';
 import 'package:futurewrold/model/student/myProject/select/SelectMyPaperFileEntity.dart';
 import 'package:futurewrold/utils/web/HttpUtils.dart';
 
@@ -22,8 +24,8 @@ class _SelectMyPaperFileState extends State<SelectMyPaperFile> {
 
   @override
   void initState() {
-    page = Icon(Icons.assignment);
-
+    page = new Center(child: Icon(Icons.block, color: Colors.red, size: 64,),);
+    getData();
   }
 
   @override
@@ -46,9 +48,32 @@ class _SelectMyPaperFileState extends State<SelectMyPaperFile> {
       method: HttpUtils.POST,
       data: selectMyPaperFileEntity.toJson(),
     );
-
+    ReturnSelectMyPaperFileEntity returnSelectMyPaperFileEntity = ReturnSelectMyPaperFileEntity.fromJson(result);
+    if (returnSelectMyPaperFileEntity.returnKey == true) {
+      List<ReturnObject> list =  returnSelectMyPaperFileEntity.returnObject;
+      if (list.length == 0) {
+        createErrorPage();
+      } else {
+        createMyPaperFileList(list);
+      }
+    } else {
+      createErrorPage();
+    }
   }
 
+  void createMyPaperFileList(List<ReturnObject> list) {
+
+    setState(() {
+      page;
+    });
+  }
+
+  void createErrorPage() {
+    page = new Center(child: Icon(Icons.block, color: Colors.red, size: 64,),);
+    setState(() {
+      page;
+    });
+  }
 
 }
 
