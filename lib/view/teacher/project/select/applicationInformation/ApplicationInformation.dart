44a78a9fef06bf8d83ba0ff951e4ed08042d3363
liupeng <liupeng.0@outlook.com>
2020-05-16 19:21:03 +0800

    教师.查看我的题目的申请状态.初始化 V 2.0.2

---
+++ lib/view/teacher/project/select/applicationInformation/ApplicationInformation.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ApplicationInformation extends StatefulWidget {
+
+
+
+  @override
+  _ApplicationInformationState createState() => _ApplicationInformationState();
+}
+
+class _ApplicationInformationState extends State<ApplicationInformation> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
