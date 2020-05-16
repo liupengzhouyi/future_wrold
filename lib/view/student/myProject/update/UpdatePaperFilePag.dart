0efeb7764694c1a9c025eae56b1bf3f314fd7d08
liupeng <liupeng.0@outlook.com>
2020-05-16 16:52:24 +0800

    学生更新论文文件.初始化 V 1.17.0

---
+++ lib/view/student/myProject/update/UpdatePaperFilePag.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class UpdatePaperFilePag extends StatefulWidget {
+  @override
+  _UpdatePaperFilePagState createState() => _UpdatePaperFilePagState();
+}
+
+class _UpdatePaperFilePagState extends State<UpdatePaperFilePag> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
