36e4ca8e3f3fe0d002b89ed6b270a8b270d3e217
liupeng <liupeng.0@outlook.com>
2020-05-16 19:27:05 +0800

    教师.查看我的题目的申请状态.请求实体 V 2.0.4

---
+++ lib/model/teacher/project/select/applicationInformation/TeacherGetApplicationInformation.dart
@@ -0,0 +1,36 @@
+class TeacherGetApplicationInformation {
+  String applicationdata;
+  int id;
+  int pass;
+  String passdata;
+  int studentid;
+  int titleid;
+
+  TeacherGetApplicationInformation(
+      {this.applicationdata,
+        this.id,
+        this.pass,
+        this.passdata,
+        this.studentid,
+        this.titleid});
+
+  TeacherGetApplicationInformation.fromJson(Map<String, dynamic> json) {
+    applicationdata = json['applicationdata'];
+    id = json['id'];
+    pass = json['pass'];
+    passdata = json['passdata'];
+    studentid = json['studentid'];
+    titleid = json['titleid'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['applicationdata'] = this.applicationdata;
+    data['id'] = this.id;
+    data['pass'] = this.pass;
+    data['passdata'] = this.passdata;
+    data['studentid'] = this.studentid;
+    data['titleid'] = this.titleid;
+    return data;
+  }
+}
